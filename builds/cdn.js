5bccae47148558ec3cfe3587c5dad132cb90e3a9
Mark Mead <itsmarkmead@gmail.com>
2022-12-08 09:16:13 +0000

    Update build script

--- builds/cdn.js
+++ builds/cdn.js
@@ -1,5 +1,3 @@
 import FILE from '../src/index.js'
 
-document.addEventListener('alpine:init', () => {
-  window.Alpine.plugin(FILE)
-})
+document.addEventListener('alpine:init', () => window.Alpine.plugin(FILE))
